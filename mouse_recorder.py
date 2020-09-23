252be50bf36135de06cbdc0b30d08cdb5a73b98b
strawberrySword <shaharglame@gmail.com>
2020-09-23 18:47:03 +0300

    Update mouse_recorder.py

--- mouse_recorder.py
+++ mouse_recorder.py
@@ -1,3 +1,10 @@
-import pyautogui
-#to set the mouse location use 
-pyautogui.moveTo(100, 100, duration = 1)
+import pyautogui, sys
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr)
+        sys.stdout.flush()
+except KeyboardInterrupt:
+    print("end")
