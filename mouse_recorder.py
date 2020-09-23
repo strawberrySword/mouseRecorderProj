ca26217456e3e9a0ee46fb5e3ffb66f2c606956e
strawberrySword <shaharglame@gmail.com>
2020-09-23 18:52:58 +0300

    Update mouse_recorder.py

--- mouse_recorder.py
+++ mouse_recorder.py
@@ -1,10 +1,1 @@
-import pyautogui, sys
-print('Press Ctrl-C to quit.')
-try:
-    while True:
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr)
-        sys.stdout.flush()
-except KeyboardInterrupt:
-    print("end")
+
